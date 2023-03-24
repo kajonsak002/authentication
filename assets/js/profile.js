7d3bfdb0f88f51e8f570c4393619c49e2fa2d283
kajonsak002 <651463002@crru.ac.th>
2023-03-24 16:33:43 +0700

    Form Login

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,20 +1,20 @@
-const user = localStorage.getItem('USER_DATA')
+const user = localStorage.getItem("USER_DATA");
 if (!user) {
-  window.location.href = 'signin.html'
+  window.location.href = "si.html";
 } else {
-  const { name, email } = JSON.parse(user)
-  const h3 = document.getElementById('user-profile')
-  h3.innerHTML = `Welcome<br>${name}`
+  const { name, email } = JSON.parse(user);
+  const h3 = document.getElementById("user-profile");
+  h3.innerHTML = `Welcome<br>${name}`;
 }
 
-const signOut = document.querySelector('#sign-out')
+const signOut = document.querySelector("#sign-out");
 
-signOut.addEventListener('click', () => {
- clearLocalStorage()
-  window.location.href = 'signin.html'
-})
+signOut.addEventListener("click", () => {
+  clearLocalStorage();
+  window.location.href = "signin.html";
+});
 
 const clearLocalStorage = () => {
-  localStorage.removeItem('AUTH_TOKEN')
-  localStorage.removeItem('USER_DATA')
-}
+  localStorage.removeItem("AUTH_TOKEN");
+  localStorage.removeItem("USER_DATA");
+};
